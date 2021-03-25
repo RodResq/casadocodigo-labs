8f92bd7a3071805d49edeb4e7e2637b592c8714b
rodrigo.resque@gmail.com <rodrigo.resque@gmail.com>
2021-03-25 20:04:55 -0300

    criacao form cadastro livro

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -2,19 +2,21 @@ package br.com.casadocodigo.loja.beans;
 
 import br.com.casadocodigo.loja.models.Livro;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named
+@RequestScoped
 public class AdminLivrosBean {
 
-    private Livro livro;
+    private Livro livro = new Livro();
 
     public Livro getLivro() {
         return livro;
     }
 
     public void salvar() {
-        System.out.println("Livro salvo com Sucesso!");
+        System.out.println("Livro cadastrado " + this.livro.toString());
 
 
     }
