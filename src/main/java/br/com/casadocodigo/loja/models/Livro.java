57d8eaf05b1d53611040c98d63fd983e6a84128d
rodrigo.resque@gmail.com <rodrigo.resque@gmail.com>
2021-03-25 12:39:56 -0300

    criacao classe Livro

--- src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -1,5 +1,21 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
+
 public class Livro {
+
+    private String titulo;
+    private String descricao;
+    private BigDecimal preco;
+    private Integer numeroPaginas;
+
+    // getters e setters aqui!
+
+    @Override
+    public String toString() {
+        return "Livro [titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco + ", numeroPaginas="
+                + numeroPaginas + "]";
+    }
+
 
 }
