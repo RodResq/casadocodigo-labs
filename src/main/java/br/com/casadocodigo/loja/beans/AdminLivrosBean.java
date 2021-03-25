7091c425d1e80bdd8b13ef6fcfc66c69d38506b3
rodrigo.resque@gmail.com <rodrigo.resque@gmail.com>
2021-03-25 12:37:36 -0300

    criacao metodo salvar

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,8 +1,14 @@
 package br.com.casadocodigo.loja.beans;
 
+import br.com.casadocodigo.loja.models.Livro;
+
 public class AdminLivrosBean {
+
+    private Livro livro;
 
     public void salvar() {
         System.out.println("Livro salvo com Sucesso!");
+
+
     }
 }
---
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -0,0 +1,4 @@
+package br.com.casadocodigo.loja.models;
+
+public class Livro {
+}
