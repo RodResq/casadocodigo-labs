26c08dea95b8f1501d27b86a751e9f3bcd0143bb
rodrigo.resque@gmail.com <rodrigo.resque@gmail.com>
2021-03-25 12:48:14 -0300

    updt

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -2,9 +2,16 @@ package br.com.casadocodigo.loja.beans;
 
 import br.com.casadocodigo.loja.models.Livro;
 
+import javax.inject.Named;
+
+@Named
 public class AdminLivrosBean {
 
     private Livro livro;
+
+    public Livro getLivro() {
+        return livro;
+    }
 
     public void salvar() {
         System.out.println("Livro salvo com Sucesso!");
